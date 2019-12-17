2c4c10154f03ba169439e77a19f7a37283fd3a99
Duter2016 <37919229+Duter2016@users.noreply.github.com>
2019-12-17 13:02:47 +0800

    Add files via upload

---
+++ precache-manifest.1efcc2347b19922371fb9fdea8bbb93f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b469d9bdd7381ef99944",
+    "url": "css/app.a6b81b55.css"
+  },
+  {
+    "revision": "97571f7412ebe564d1f1",
+    "url": "css/chunk-vendors.d39e9fad.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "9e9a91d443c0c7e4bf9d7fea2e623f6b",
+    "url": "index.html"
+  },
+  {
+    "revision": "b469d9bdd7381ef99944",
+    "url": "js/app.db1511bf.js"
+  },
+  {
+    "revision": "97571f7412ebe564d1f1",
+    "url": "js/chunk-vendors.0cd37ec8.js"
+  },
+  {
+    "revision": "28f3e13ec88073aa1b85b7d66358f613",
+    "url": "manifest.json"
+  }
+]);
